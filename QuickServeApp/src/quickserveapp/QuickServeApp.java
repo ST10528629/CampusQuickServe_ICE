c16de3e00129cae3025cb4a5747d9b9a865f9d63
lab_services_student <lab_services_student@LabVM2043622.ytddryjtiquebbp4ixqhdwnr0h.jnbx.internal.cloudapp.net>
2026-03-04 10:41:00 +0200

    my initial commit

---
+++ QuickServeApp/src/quickserveapp/QuickServeApp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quickserveapp;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class QuickServeApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
