e63118cd2a29ba0180571eb47b608c47d9b594e0
lab_services_student <lab_services_student@LabVM2043622.ytddryjtiquebbp4ixqhdwnr0h.jnbx.internal.cloudapp.net>
2026-03-06 23:33:42 +0200

    Attempted to instead try to rewrite the code to include a selectable menu

--- QuickServeApp/src/quickserveapp/QuickServeApp.java
+++ QuickServeApp/src/quickserveapp/QuickServeApp.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package quickserveapp;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.JOptionPane;
         import order.Order;
 
@@ -11,12 +12,20 @@ import javax.swing.JOptionPane;
 public class QuickServeApp {
  
     public static void main(String[] args) {
+        Map<String, Double> menu = new HashMap<>();
+            menu.put ("Burger", 45.00);
+            menu.put ("Pizza", 35.00);
+            menu.put ("Coffee", 15.00);
+            
+        
         ///User Inputs
-      
-
        String customerName = JOptionPane.showInputDialog(null, "please enter your name");
        String studentId = JOptionPane.showInputDialog(null, "please enter your student ID");
-       String selectedOrder = JOptionPane.showInputDialog(null, "Enter selected item");
+       
+       Object[] options = menu.keySet().toArray();
+       String SelectedItem = (String) JOptionPane.showInputDialog(null, "select an item:", "menu", JOptionPane.PLAIN_MESSAGE, null, options,
+       options[0]
+       );
        
        int quantity = 0;
        double pricePerItem = 0.0;
