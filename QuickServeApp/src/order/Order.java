aee3abd8b86c453f380b372978267aa2d9817aa9
lab_services_student <lab_services_student@LabVM2043622.ytddryjtiquebbp4ixqhdwnr0h.jnbx.internal.cloudapp.net>
2026-03-06 22:24:49 +0200

    Added imports

--- QuickServeApp/src/order/Order.java
+++ QuickServeApp/src/order/Order.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package order;
-
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 /**
  *
  * @author lab_services_student
